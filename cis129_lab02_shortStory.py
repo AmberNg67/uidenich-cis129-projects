349d987aab0953b5aac93236c2fa813eb38c8872
AmberNg67 <160768494+AmberNg67@users.noreply.github.com>
2024-02-21 17:05:40 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,8 +1,8 @@
 # Author: Amber Uidenich
 # My first Python lab in my CIS129 course
 # Prints a math limerick
-Print("A dozen, a gross, and a score")
-Print(:Plus three times the square root of four")
-Print("Divided by seven")
-Print("Plus five times eleven")
-Print("Is nine squared and not a bit more.")
+print("A dozen, a gross, and a score")
+print(:Plus three times the square root of four")
+print("Divided by seven")
+print("Plus five times eleven")
+print("Is nine squared and not a bit more.")
