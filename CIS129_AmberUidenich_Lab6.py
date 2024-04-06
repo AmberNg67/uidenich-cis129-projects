9edeb1488c7981c607f188f5c49c963c14d2209a
AmberNg67 <160768494+AmberNg67@users.noreply.github.com>
2024-04-06 09:09:30 -0700

    Create CIS129_AmberUidenich_Lab6.py
    This is the work for Module 6 Lab.
    The code calculates the number of hotdogs needed for a cookout based on number of people and a hot dogs per person count. It then takes that number and calculates the minimum number of packages of hotdogs and buns need to be purchased. Finally, it calculates how many hotdogs and buns will be left over.

---
+++ CIS129_AmberUidenich_Lab6.py
@@ -0,0 +1,36 @@
+# CIS129 Module 6 Lab
+# Amber Uidenich
+# Main Module (using MOD)
+# Determine the total number of hot dogs needed
+
+def getTotalHotDogs():
+    # Determine the total number of hot dogs needed
+    people = 0 # Number of people attending the cookout
+    hotdogsPerPerson = 0 # Number of hotdogs that will be served to each person
+    people = int(input("How many people will be attending the cookout? "))
+    hotdogsPerPerson = int(input("How many hot dogs will you serve each person? "))
+    totalDogsNeeded = people * hotdogsPerPerson
+    print("You will need", totalDogsNeeded, "hotdogs for the cookout.")
+    
+    #Determine the minimum number of packages of hotdogs needed
+    DOGS = 8 # Number of buns in a package
+    import math
+    minDogsNeeded = math.ceil(totalDogsNeeded / DOGS)
+    print("You will need", minDogsNeeded, "packages of hotdogs.")
+    
+    #Determine the minimum number of packages of buns needed
+    BUNS = 10 # Number of buns in a package
+    bunsNeeded = totalDogsNeeded # The number of buns needed will equal the number of hotdogs served
+    minBunsNeeded = math.ceil(bunsNeeded / BUNS)
+    print("You will need", minBunsNeeded, "packages of buns.")
+    
+    #Determine number of hotdogs left over
+    dogsLeft = (DOGS - totalDogsNeeded % DOGS) % DOGS
+    print("You will have", dogsLeft, "hotdogs left over.")
+
+    
+    #Determine number of buns left over
+    bunsLeft = (BUNS - bunsNeeded % BUNS) % BUNS
+    print("You will have", bunsLeft, "buns left over.")
+    
+getTotalHotDogs()
